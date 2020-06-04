83f8ed771b12b001c4fa4b32a940486af3a28bae
yuanhao <wuyhthu@gmail.com>
2020-06-04 14:24:19 +0800

    simplify usage

--- inferlight/__init__.py
+++ inferlight/__init__.py
@@ -1,3 +1,2 @@
-from inferlight.http_frontend import app
 from inferlight.handler import Handler
 from inferlight.inferlight import InferLight
--- inferlight/http_frontend.py
+++ inferlight/http_frontend.py
@@ -1,7 +1,7 @@
 from sanic import Sanic
 from sanic.log import logger
 from sanic.response import json, text
-
+from asyncio import sleep
 
 app = Sanic("QA inference")
 
@@ -13,3 +13,16 @@ async def notify_server_stopping(app, loop):
 @app.route("/")
 async def test(request):
     return text("I am alive!")
+
+
+async def predict(request, data_dispatch_fn=None):
+    if data_dispatch_fn is None:
+        raise ValueError("data_dispatch_fn should be implemented!")
+    task_data = data_dispatch_fn(request)
+    task_id = light.put_task(task_data)
+    while True:
+        result = light.get_result(task_id)
+        if result is not None:
+            break
+        await sleep(0.005)
+    return json({"result": str(result)})
